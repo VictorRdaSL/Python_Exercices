d16d1ac98d353727e0356052a1412ea3018623e0
Victor Rubens da Silva Lima <victorsilvalima1@hotmail.com>
2026-04-05 13:10:49 -0300

    Update Problema A.py

--- Problema A.py
+++ Problema A.py
@@ -1,3 +1,17 @@
+#PROBLEMA A - Uma questão similar ao INTERFATECS
+# Um conjunto de três números naturais pode formar um triângulo se, para cada três números a, b e
+# c, a soma de quaisquer dois números for maior que o terceiro. Por exemplo, o conjunto {3, 4, 5}
+# pode formar um triângulo, mas o conjunto {1, 2, 5} não pode.
+# Desenvolva um programa que, dado três números naturais, determine se eles podem formar um triângulo.
+# triângulo.
+
+# Entrada
+# Três números naturais separados por espaço representando os comprimentos dos lados do triângulo.
+
+# Saída
+# Se os três números puderem formar um triângulo, imprima "Sim". Caso contrário, imprima "Não".
+
+
 n1 = int(input('Digite um numero:'))
 n2 = int(input('Digite outro numero:'))
 n3 = int(input('Digite outro numero: '))
